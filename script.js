2d135e06451854b4a29e797899a008a72a2f3223
Blastonn <felipe.paulo07@hotmail.com>
2025-04-08 11:52:56 -0300

    Feat - Adicionar funcao de operacao
    Adicionada funcao para realizacao de escolha
    e operacoes matematicas.

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-function soma(...numeros){
-    const resultado = numeros.reduce((total,atual) => total + atual,0);
-    console.log(resultado);
+function soma(op = "+",...numeros){
+    const resultado = numeros.reduce((total,atual) => {
+        if(op  === "+") return total + atual;
+    },0);
+    return resultado;
 }
 
-function subtracao(...numeros){
-    const resultado = numeros.reduce((total,atual) => total - atual,0);
-    console.log(resultado);
+function subtracao(op = "-",...numeros){
+    const resultado = numeros.reduce((total,atual) => {
+        if(op  === "-") return total - atual;
+    });
+    return resultado;
 }
 
-function multiplicacao(...numeros){
-    const resultado = numeros.reduce((total,atual) => total * atual);
-    console.log(resultado);
+function multiplicacao(op = "*",...numeros){
+    const resultado = numeros.reduce((total,atual) => {
+        if(op  === "*") return total * atual;
+    });
+    return resultado;
 }
 
-function divisao(...numeros){
-    const resultado = numeros.reduce((total,atual) => total / atual);
-    console.log(resultado);
+function divisao(op = "/",...numeros){
+    const resultado = numeros.reduce((total,atual) => {
+        if(op  === "/") return total / atual;
+    });
+    return resultado;
 }
+
+function operacao(oper, num, num2){
+   if(oper === "+"){
+        const resultadoSoma = soma(oper,num,num2);
+        return resultadoSoma;
+   }else if(oper === "-"){
+        const resultadoSub = subtracao(oper,num,num2);
+        return resultadoSub;
+   }else if(oper === "*"){
+        const resultadoMult = multiplicacao(oper,num,num2);
+        return resultadoMult
+   }else if(oper === "/"){
+        const resultadoDivi = divisao(oper,num,num2);
+        return resultadoDivi;
+   }
+}
+
+operacao("/", 5,2);
